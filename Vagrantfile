99b98b8ac407bd023b37dd36400980180c7465fe
Daniel Ehlers <danielehlers@mindeye.net>
2014-11-09 02:05:40 +0100

    Add vagrantfile

---
+++ Vagrantfile
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+
+  # Every Vagrant virtual environment requires a box to build off of.
+  config.vm.box = "box-cutter/debian77"
+
+  config.vm.provider "virtualbox" do |vb|
+    # Don't boot with headless mode
+    # vb.gui = true
+
+    # Use VBoxManage to customize the VM. For example to change memory:
+    vb.customize ["modifyvm", :id, "--memory", "1024"]
+  end
+
+  # Gotham City Gateways
+  (0..4).each do |i|
+		config.vm.define "gc-gw#{i}" do |node|
+			node.vm.hostname = "gc-gw#{i}"
+			node.vm.network "private_network", ip: "172.19.1.#{i+1}", netmask: "255.255.0.0"
+			node.vm.provision :shell, path: "bootstrap.sh", args: "gc-gw#{i}"
+		end
+  end
+
+
+end
---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+MACHINE=$1
+
+cat > /etc/apt/sources.list << EOF
+deb http://ftp.de.debian.org/debian wheezy main
+deb-src http://ftp.de.debian.org/debian wheezy main
+
+deb http://security.debian.org/ wheezy/updates main contrib
+deb-src http://security.debian.org/ wheezy/updates main contrib
+
+# wheezy-updates, previously known as 'volatile'
+deb http://ftp.de.debian.org/debian wheezy-updates main contrib
+deb-src http://ftp.de.debian.org/debian wheezy-updates main contrib
+EOF
+
+apt-get update
+apt-get install --no-install-recommends -y puppet git tcpdump mtr-tiny
+
+puppet module install puppetlabs-stdlib
+puppet module install puppetlabs-apt
+puppet module install puppetlabs-vcsrepo
+
+cd /etc/puppet/modules
+git clone https://github.com/ffnord/ffnord-puppet-gateway ffnord
+
+cd "/vagrant/machines/${MACHINE}/"
+cp * /root
+cd /root
+puppet apply manifest.pp --verbose
+
+build-firewall
+service iptables-persistent save
